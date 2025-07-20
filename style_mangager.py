def5ddc9d79f187a22bd17d581bead9144db7e6c
Dave Valdez <david.b.valdez@gmail.com>
2025-07-20 14:18:57 -0600

    Create style_mangager.py

---
+++ style_mangager.py
@@ -0,0 +1,17 @@
+# style_manager.py
+import streamlit as st
+
+def inject_custom_css():
+    st.markdown("""
+        <style>
+            .stButton button {
+                background-color: #4CAF50;
+                color: white;
+                font-weight: bold;
+                border-radius: 6px;
+            }
+            .stWarning {
+                font-size: 1rem;
+            }
+        </style>
+    """, unsafe_allow_html=True)
